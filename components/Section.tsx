89bdae985c67792cab3e043d5d156684c295b9a4
Sumeja <sumeja.buljugija@gmail.com>
2024-09-22 11:49:06 +0200

    responsive design

--- components/Section.tsx
+++ components/Section.tsx
@@ -8,26 +8,29 @@ interface SectionProps {
   button2Text: string;
 }
 
-const Section: React.FC<SectionProps> = ({ 
-  backgroundImageClass, 
-  header, 
-  miniHeader, 
-  button1Text, 
-  button2Text 
+const Section: React.FC<SectionProps> = ({
+  backgroundImageClass,
+  header,
+  miniHeader,
+  button1Text,
+  button2Text,
 }) => {
   return (
     <div
       className={`h-screen w-full relative bg-cover bg-center flex flex-col justify-between text-center text-white ${backgroundImageClass}`}
     >
-      <div className="flex-grow flex items-center justify-center top-0 -mt-56">
-      <div>
-            <h1 className="text-5xl font-extrabold text-slate-100">{header}</h1>
-            <p className="text-lg font-semibold mt-2 relative inline-block after:content-[''] after:absolute after:left-0 after:bottom-[-4px] after:w-full after:h-[2px] after:bg-white">
-      {miniHeader}
-    </p>
-  </div>
+      {/* Header Section */}
+      <div className="flex-grow flex items-center justify-center -mt-56 md:-mt-24">
+        <div>
+          <h1 className="text-3xl md:text-5xl font-extrabold text-slate-100">{header}</h1>
+          <p className="text-md md:text-lg font-semibold mt-2 relative inline-block after:content-[''] after:absolute after:left-0 after:bottom-[-4px] after:w-full after:h-[2px] after:bg-white">
+            {miniHeader}
+          </p>
+        </div>
       </div>
-      <div className="mb-10 space-x-4 max-w-5xl justify-center mx-auto z-10">
+
+      {/* Button Section */}
+      <div className="mb-10 space-x-0 space-y-4 md:space-x-4 md:space-y-0 max-w-5xl justify-center mx-auto z-10 flex flex-col md:flex-row">
         <button className="bg-blue-500 text-white min-w-[250px] py-2 px-6 rounded-lg hover:bg-gray-800 transition">
           {button1Text}
         </button>
@@ -37,6 +40,6 @@ const Section: React.FC<SectionProps> = ({
       </div>
     </div>
   );
-}
+};
 
 export default Section;
